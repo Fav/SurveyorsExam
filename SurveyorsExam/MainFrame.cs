cf456d3205197fe9af6649bb5c0422ad5353d6b1
fav <f25457@qq.com>
2015-06-19 17:26:43 +0800

    Signed-off-by: fav <f25457@qq.com>first huqi

---
+++ SurveyorsExam/MainFrame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace SurveyorsExam
+{
+    public partial class MainFrame : Form
+    {
+        FrmAnswer frmAnswer = new FrmAnswer();
+        FrmTitle frmTitle = new FrmTitle();
+        public MainFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowFrm(DockContent frm)
+        {
+            frm.CloseButton = false;
+            frm.HideOnClose = true;
+            frm.Show(this.dockPanel1);
+        }
+
+        private void MainFrame_Load(object sender, EventArgs e)
+        {
+            ShowFrm(frmAnswer);
+            ShowFrm(frmTitle);
+        }
+    }
+}
